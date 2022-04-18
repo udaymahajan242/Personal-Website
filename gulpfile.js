6b13e2c8a00d1855411292adcc36027b38025d59
Uday Mahajan <udaym242@uw.edu>
2022-04-18 14:44:42 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@ const sass = require('./build/sass');
 const scripts = require('./build/scripts');
 const images = require('./build/images');
 const sync = require('./build/browsersync');
-
+var exec = require('child_process').exec;
+var gutil = require('gulp-util');
 [sass, scripts, images, sync].forEach(task => {
   task(gulp);
 });
 
-var jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+gulp.task('build', gulp.series(['sass', 'scripts', 'images']));
 
-gulp.task('build', gulp.series(['sass', 'scripts', 'images', 'jekyll']));
+
+gulp.task('jekyll', function (){
+exec('jekyll build', function(err, stdout, stderr) {
+    console.log(stdout);
+});
+});
